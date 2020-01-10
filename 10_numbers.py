1ce64309faf901034a5ef60654c105249f73ecb5
Prafful Daga <prraful@gmail.com>
2020-01-10 12:59:05 +0530

    python numbers

---
+++ 10_numbers.py
@@ -0,0 +1,22 @@
+
+import math
+
+num1 =8
+num2  = 88.8
+num3 = 945647985413135469765131
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+num4 = num1   + num2
+print(type(num4))
+
+num5 = 88.4587541
+print(num5)
+print(round(num5, 1))
+print(round(num5,-2))
+print(round(num5))
+
+print(math.ceil(num5))
+print(math.floor(num5))
