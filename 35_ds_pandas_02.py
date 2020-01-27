4739e1376080933d52ceb7fd47e7006906192684
Prafful Daga <prraful@gmail.com>
2020-01-27 12:07:49 +0530

    dataframe as in pandas 02

---
+++ 35_ds_pandas_02.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+df = pd.read_csv('weather_data.csv')
+df
+
+
+# In[5]:
+
+
+weather_data = {
+'day':['01-01-2017','01-02-2017','01-03-2017','01-04-2017','01-05-2017','01-06-2017'],
+'temperature':[32,35,28,24,32,31],
+'event':['Rain','Sunny','Snow','Snow','Rain','Sunny']
+}
+df = pd.DataFrame(weather_data)
+df
+
+
+# In[6]:
+
+
+df.shape
+
+
+# In[7]:
+
+
+df.head()
+
+
+# In[8]:
+
+
+df.head(2)
+
+
+# In[9]:
+
+
+df.tail()
+
+
+# In[10]:
+
+
+df.tail(2)
+
+
+# In[11]:
+
+
+df[3:6]
+
+
+# In[13]:
+
+
+df[0:2]
+
+
+# In[14]:
+
+
+df[:]
+
+
+# In[16]:
+
+
+df
+
+
+# In[17]:
+
+
+df.columns
+
+
+# In[18]:
+
+
+df['day']
+
+
+# In[19]:
+
+
+type(df['day'])
+
+
+# In[20]:
+
+
+df[['day', 'event']]
+
+
+# In[22]:
+
+
+df['temperature']
+
+
+# In[23]:
+
+
+df['temperature'].max()
+
+
+# In[24]:
+
+
+df[df['temperature']>=31]
+
+
+# In[26]:
+
+
+df[df['temperature']>31]
+
+
+# In[28]:
+
+
+df[df['temperature']==df['temperature'].max()]
+
+
+# In[29]:
+
+
+df[df['temperature'] == df['temperature'].min()]
+
+
+# In[30]:
+
+
+df.describe()
+
+
+# In[31]:
+
+
+df = pd.read_csv("weather_data.csv")
+
+
+# In[32]:
+
+
+df.describe()
+
+
+# In[33]:
+
+
+df
+
